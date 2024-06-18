b482cbce177bf440bd8bfe7d8ffa04ad997b1780
Mirko Dietrich <dietrich@math.tu-berlin.de>
2024-06-18 14:52:23 +0200

    chore: fix inconsistent license headers

--- tests/webserver/conftest.py
+++ tests/webserver/conftest.py
@@ -1,5 +1,5 @@
-#  This file is part of the QuestionPy Server. (https://questionpy.org)
-#  The QuestionPy Server is free software released under terms of the MIT license. See LICENSE.md.
+#  This file is part of the QuestionPy SDK. (https://questionpy.org)
+#  The QuestionPy SDK is free software released under terms of the MIT license. See LICENSE.md.
 #  (c) Technische Universität Berlin, innoCampus <info@isis.tu-berlin.de>
 
 import asyncio
