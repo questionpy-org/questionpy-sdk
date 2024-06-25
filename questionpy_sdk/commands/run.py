ca0da8a4b81b78fcb25b5b3be78a4cad066e4178
Mirko Dietrich <dietrich@math.tu-berlin.de>
2024-06-25 11:16:42 +0200

    chore: remove `FunctionPackageLocation` from supported locations for run command
    Also, rename `infer_package_kind` to `get_package_location`.

--- questionpy_sdk/commands/run.py
+++ questionpy_sdk/commands/run.py
@@ -4,12 +4,20 @@
 
 import click
 
-from questionpy_sdk.commands._helper import infer_package_kind
+from questionpy_sdk.commands._helper import get_package_location
 from questionpy_sdk.webserver.app import WebServer
 
 
 @click.command()
 @click.argument("package")
 def run(package: str) -> None:
-    web_server = WebServer(infer_package_kind(package))
+    """Run a package.
+
+    \b
+    PACKAGE can be:
+    - a .qpy file,
+    - a dist directory, or
+    - a source directory (built on-the-fly).
+    """  # noqa: D301
+    web_server = WebServer(get_package_location(package))
     web_server.start_server()
