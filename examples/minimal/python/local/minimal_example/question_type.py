54492edfd6818cafe6af8716abb81befb0aebe30
Jan Britz <britz@tu-berlin.de>
2024-10-29 12:51:21 +0100

    fix: formulation template name

--- examples/minimal/python/local/minimal_example/question_type.py
+++ examples/minimal/python/local/minimal_example/question_type.py
@@ -1,5 +1,6 @@
 from questionpy import Attempt, Question, ResponseNotScorableError
 
+from .constants import NAMESPACE, SHORT_NAME
 from .form import MyModel
 
 
@@ -17,7 +18,7 @@ class ExampleAttempt(Attempt):
     @property
     def formulation(self) -> str:
         self.placeholders["description"] = "Welcher ist der zweite Buchstabe im deutschen Alphabet?"
-        return self.jinja2.get_template("local.minimal_example/formulation.xhtml.j2").render()
+        return self.jinja2.get_template(f"{NAMESPACE}.{SHORT_NAME}/formulation.xhtml.j2").render()
 
 
 class ExampleQuestion(Question):
