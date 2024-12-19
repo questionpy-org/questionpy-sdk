fb2a966427c1940f2f248cfcd41e0f7dbe3e1816
Maximilian Haye <m.haye@tu-berlin.de>
2024-12-19 19:04:17 +0100

    feat: add generated_id dsl element
    See: #140

--- questionpy/_util.py
+++ questionpy/_util.py
@@ -5,6 +5,7 @@ _T = TypeVar("_T", bound=type)
 
 
 def get_mro_type_hint(klass: type, attr_name: str, bound: _T) -> _T:
+    # FIXME: This function is called too early, when the classes referenced in forward refs may not be defined yet.
     for superclass in klass.mro():
         hints = get_type_hints(superclass)
         if attr_name in hints:
