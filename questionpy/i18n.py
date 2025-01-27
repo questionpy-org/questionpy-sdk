d38c7e2d206e7c5dbf3adcf205db5ecd201db088
Maximilian Haye <m.haye@tu-berlin.de>
2025-01-27 14:11:46 +0100

    fix(i18n): use a ContextVar for the current state

--- questionpy/i18n.py
+++ questionpy/i18n.py
@@ -1,4 +1,5 @@
 import logging
+from contextvars import ContextVar
 from gettext import GNUTranslations, NullTranslations
 from importlib.resources.abc import Traversable
 
@@ -8,9 +9,9 @@ from questionpy_common.manifest import SourceManifest
 DEFAULT_CATEGORY = "LC_MESSAGES"
 _NULL_TRANSLATIONS = NullTranslations()
 
-_STATE: tuple[str, NullTranslations] | None = None
+_i18n_state: ContextVar[tuple[str, NullTranslations]] = ContextVar(f"_i18n_state")
 
-log = logging.getLogger(__name__)
+_log = logging.getLogger(__name__)
 
 
 def domain_of(manifest: SourceManifest) -> str:
@@ -58,50 +59,50 @@ def _get_available_mos(package: Package) -> dict[str, Traversable]:
 
 def get_state() -> tuple[str, NullTranslations]:
     """Get the current i18n state of the worker."""
-    if not _STATE:
+    state = _i18n_state.get(None)
+    if not state:
         msg = f"i18n was not initialized. Call {__name__}.{initialize.__name__} first."
         raise RuntimeError(msg)
-    return _STATE
+    return state
 
 
 def is_initialized() -> bool:
-    return _STATE is not None
+    return _i18n_state.get(None) is not None
 
 
 def initialize(package: Package, env: Environment) -> None:
     # PLW0603 discourages the global statement, but the alternative would be much less readable.
     # ruff: noqa: PLW0603
 
-    global _STATE
-    if _STATE:
+    if is_initialized():
         # Prevent multiple initializations, which would add multiple on_request_callbacks overwriting each other.
         return
 
     untranslated_lang = _guess_untranslated_language(package)
     _NULL_TRANSLATIONS.install()
-    _STATE = (untranslated_lang, _NULL_TRANSLATIONS)
+    _i18n_state.set((untranslated_lang, _NULL_TRANSLATIONS))
 
     available_mos = _get_available_mos(package)
 
     if not available_mos:
         # We'd never translate anything anyway, prepare_i18n would install the NullTranslations every time.
         # (Ohne MOs nix los)
-        log.debug(
+        _log.debug(
             "No MO files found, messages will not be translated. We'll assume the untranslated strings to be in '%s'.",
             untranslated_lang,
         )
         return
 
-    log.debug("Found MO files for the following languages: %s", ", ".join(available_mos.keys()))
+    _log.debug("Found MO files for the following languages: %s", ", ".join(available_mos.keys()))
 
     def prepare_i18n(request_user: RequestUser) -> None:
         langs_to_use = [lang for lang in request_user.preferred_languages if lang in available_mos]
 
         if langs_to_use:
-            log.debug("Using the following languages for this request: %s", langs_to_use)
+            _log.debug("Using the following languages for this request: %s", langs_to_use)
             primary_lang = langs_to_use[0]
         else:
-            log.debug(
+            _log.debug(
                 "There are no MO files for any of the user's preferred languages. Messages will not be translated "
                 "and we'll assume the untranslated strings to be in '%s'.",
                 untranslated_lang,
@@ -111,8 +112,7 @@ def initialize(package: Package, env: Environment) -> None:
         translations = _build_translations([available_mos[lang] for lang in langs_to_use])
         translations.install()
 
-        global _STATE
-        _STATE = primary_lang, translations
+        _i18n_state.set((primary_lang, translations))
 
     env.register_on_request_callback(prepare_i18n)
 
