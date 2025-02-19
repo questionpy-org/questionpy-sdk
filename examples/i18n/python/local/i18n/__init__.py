77ad15f3ee1c69dc981ae5e95b9d65baad9c7c0e
Maximilian Haye <m.haye@tu-berlin.de>
2025-02-19 15:03:01 +0100

    feat(i18n): separate state by domain

--- examples/i18n/python/local/i18n/__init__.py
+++ examples/i18n/python/local/i18n/__init__.py
@@ -1,17 +1,17 @@
 #  This file is part of the QuestionPy SDK. (https://questionpy.org)
 #  The QuestionPy SDK is free software released under terms of the MIT license. See LICENSE.md.
 #  (c) Technische Universität Berlin, innoCampus <info@isis.tu-berlin.de>
-from questionpy import Attempt, NeedsManualScoringError, Question, make_question_type_init
+from questionpy import Attempt, NeedsManualScoringError, Question, i18n, make_question_type_init
 from questionpy.form import FormModel, static_text
-from questionpy.i18n import gettext as _
 from questionpy_common.elements import StaticTextElement
 
-
-def _(a):
-    return a
+_, _N = i18n.get_for(__package__)
 
 
 class I18NModel(FormModel):
+    # TODO: Implement deferred translation.
+    _ = lambda x: x  # noqa: E731
+
     txt: StaticTextElement = static_text(
         # TRANSLATORS: This comment will be shown in the .pot file.
         _("Important Notice"),
@@ -26,6 +26,10 @@ class I18NAttempt(Attempt):
     @property
     def formulation(self) -> str:
         return self.jinja2.get_template("formulation.xhtml.j2").render()
+
+    @property
+    def general_feedback(self) -> str | None:
+        return "<div>" + _("It's all wrong!") + "</div>"
 
 
 class I18NQuestion(Question):
--- questionpy/i18n.py
+++ questionpy/i18n.py
@@ -1,21 +1,45 @@
 import logging
+from collections.abc import Callable
 from contextvars import ContextVar
+from dataclasses import dataclass
 from gettext import GNUTranslations, NullTranslations
 from importlib.resources.abc import Traversable
+from typing import TypeAlias
 
-from questionpy_common.environment import Environment, Package, RequestUser
+from questionpy_common.environment import (
+    Environment,
+    Package,
+    PackageNamespaceAndShortName,
+    RequestUser,
+    get_qpy_environment,
+)
 from questionpy_common.manifest import SourceManifest
 
 DEFAULT_CATEGORY = "LC_MESSAGES"
 _NULL_TRANSLATIONS = NullTranslations()
 
-_i18n_state: ContextVar[tuple[str, NullTranslations]] = ContextVar(f"_i18n_state")
+
+@dataclass
+class _RequestState:
+    user: RequestUser
+    primary_lang: str
+    translations: NullTranslations
+
+
+@dataclass
+class _DomainState:
+    untranslated_lang: str
+    available_mos: dict[str, Traversable]
+    request_state: _RequestState | None = None
+
+
+_i18n_state: ContextVar[dict[str, _DomainState]] = ContextVar("_i18n_state")
 
 _log = logging.getLogger(__name__)
 
 
-def domain_of(manifest: SourceManifest) -> str:
-    return f"{manifest.namespace}.{manifest.short_name}"
+def domain_of(package: SourceManifest | PackageNamespaceAndShortName) -> str:
+    return f"{package.namespace}.{package.short_name}"
 
 
 def _guess_untranslated_language(package: Package) -> str:
@@ -57,46 +81,80 @@ def _get_available_mos(package: Package) -> dict[str, Traversable]:
     return result
 
 
-def get_state() -> tuple[str, NullTranslations]:
-    """Get the current i18n state of the worker."""
-    state = _i18n_state.get(None)
-    if not state:
-        msg = f"i18n was not initialized. Call {__name__}.{initialize.__name__} first."
+def _require_request_user() -> RequestUser:
+    env = get_qpy_environment()
+    if not env.request_user:
+        msg = "No request is currently being processed."
         raise RuntimeError(msg)
-    return state
+
+    return env.request_user
 
 
-def is_initialized() -> bool:
-    return _i18n_state.get(None) is not None
+def _require_request_state(domain: str, domain_state: _DomainState) -> _RequestState:
+    request_user = _require_request_user()
+    if not domain_state.request_state or domain_state.request_state.user != request_user:
+        msg = f"i18n domain '{domain}' was not initialized for the current request."
+        raise RuntimeError(msg)
+
+    return domain_state.request_state
 
 
-def initialize(package: Package, env: Environment) -> None:
-    # PLW0603 discourages the global statement, but the alternative would be much less readable.
-    # ruff: noqa: PLW0603
+def get_translations_of_package(package: Package) -> NullTranslations:
+    """Get the current i18n state of the worker."""
+    domain = domain_of(package.manifest)
+    domain_state = _ensure_initialized(domain, package, get_qpy_environment())
+    request_state = _require_request_state(domain, domain_state)
+    return request_state.translations
 
-    if is_initialized():
-        # Prevent multiple initializations, which would add multiple on_request_callbacks overwriting each other.
-        return
+
+_GettextFun: TypeAlias = Callable[[str], str]
+
+
+def _get_package_owning_module(module_name: str) -> Package:
+    # TODO: Dedupe when #152 is in dev.
+    try:
+        namespace, short_name, *_ = module_name.split(".", maxsplit=2)
+        env = get_qpy_environment()
+        key = PackageNamespaceAndShortName(namespace=namespace, short_name=short_name)
+        return env.packages[key]
+    except (KeyError, ValueError) as e:
+        msg = (
+            "Current package namespace and shortname could not be determined from '__module__' attribute. Please do "
+            "not modify the '__module__' attribute."
+        )
+        raise ValueError(msg) from e
+
+
+def _ensure_initialized(domain: str, package: Package, env: Environment) -> _DomainState:
+    states_by_domain = _i18n_state.get(None)
+    if states_by_domain is None:
+        states_by_domain = {}
+        _i18n_state.set(states_by_domain)
+
+    domain_state = states_by_domain.get(domain)
+    if domain_state:
+        # Already initialized.
+        return domain_state
 
     untranslated_lang = _guess_untranslated_language(package)
-    _NULL_TRANSLATIONS.install()
-    _i18n_state.set((untranslated_lang, _NULL_TRANSLATIONS))
-
     available_mos = _get_available_mos(package)
-
-    if not available_mos:
-        # We'd never translate anything anyway, prepare_i18n would install the NullTranslations every time.
-        # (Ohne MOs nix los)
+    if available_mos:
         _log.debug(
-            "No MO files found, messages will not be translated. We'll assume the untranslated strings to be in '%s'.",
+            "For domain '%s', MO files for the following languages were found: %s",
+            domain,
+            ", ".join(available_mos.keys()),
+        )
+    else:
+        _log.debug(
+            "For domain '%s', no MO files were found. Messages will not be translated. We'll assume the "
+            "untranslated strings to be in '%s'.",
             untranslated_lang,
         )
-        return
 
-    _log.debug("Found MO files for the following languages: %s", ", ".join(available_mos.keys()))
+    domain_state = states_by_domain[domain] = _DomainState(untranslated_lang, available_mos)
 
-    def prepare_i18n(request_user: RequestUser) -> None:
-        langs_to_use = [lang for lang in request_user.preferred_languages if lang in available_mos]
+    def initialize_for_request(request_user: RequestUser) -> None:
+        langs_to_use = [lang for lang in request_user.preferred_languages if lang in domain_state.available_mos]
 
         if langs_to_use:
             _log.debug("Using the following languages for this request: %s", langs_to_use)
@@ -105,21 +163,36 @@ def initialize(package: Package, env: Environment) -> None:
             _log.debug(
                 "There are no MO files for any of the user's preferred languages. Messages will not be translated "
                 "and we'll assume the untranslated strings to be in '%s'.",
-                untranslated_lang,
+                domain_state.untranslated_lang,
             )
-            primary_lang = untranslated_lang
+            primary_lang = domain_state.untranslated_lang
 
-        translations = _build_translations([available_mos[lang] for lang in langs_to_use])
-        translations.install()
+        translations = _build_translations([domain_state.available_mos[lang] for lang in langs_to_use])
+        domain_state.request_state = _RequestState(request_user, primary_lang, translations)
 
-        _i18n_state.set((primary_lang, translations))
+    if env.request_user:
+        # In case we are called during a request.
+        initialize_for_request(env.request_user)
 
-    env.register_on_request_callback(prepare_i18n)
+    env.register_on_request_callback(initialize_for_request)
+
+    return domain_state
 
 
-def gettext(msgid: str) -> str:
-    _, translations = get_state()
-    return translations.gettext(msgid)
+def get_for(module_name: str) -> tuple[_GettextFun, _GettextFun]:
+    # TODO: Maybe cache this?
+    package = _get_package_owning_module(module_name)
+    domain = domain_of(package.manifest)
+    domain_state = _ensure_initialized(domain, package, get_qpy_environment())
+
+    def gettext(message: str) -> str:
+        request_state = _require_request_state(domain, domain_state)
+        return request_state.translations.gettext(message)
+
+    def ngettext(message: str) -> str:
+        return message
+
+    return gettext, ngettext
 
 
-__all__ = ["DEFAULT_CATEGORY", "domain_of", "get_state", "gettext", "initialize", "is_initialized"]
+__all__ = ["DEFAULT_CATEGORY", "domain_of", "get_for", "get_translations_of_package"]
