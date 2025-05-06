eea89967913c3953e0b36e89a44bb13791dc2446
Mirko Dietrich <dietrich@math.tu-berlin.de>
2025-05-06 19:02:54 +0200

    test: run Node.js checks and E2E tests in GitHub workflow

--- run_e2e_tests.sh
+++ run_e2e_tests.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-# https://playwright.dev/python/docs/test-runners#async-fixtures
-exec pytest \
-    --override-ini asyncio_default_test_loop_scope=session \
-    --override-ini asyncio_default_fixture_loop_scope=session \
-    e2e/ \
-    "$@"
+exec pytest --config-file=e2e/pytest.ini "$@"
