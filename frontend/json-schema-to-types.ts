974234bb238e87dd0281be4f805928792f6ef47b
Mirko Dietrich <dietrich@math.tu-berlin.de>
2025-06-12 16:09:58 +0200

    chore(frontend): avoid TSDoc-style headers in `.ts` files
    Replaced `/**` with `/*` in file headers to prevent misinterpretation as TSDoc blocks when no
    imports precede a declaration.

--- frontend/json-schema-to-types.ts
+++ frontend/json-schema-to-types.ts
@@ -1,4 +1,4 @@
-/**
+/*
  * This file is part of the QuestionPy SDK. (https://questionpy.org)
  * The QuestionPy SDK is free software released under terms of the MIT license. See LICENSE.md.
  * (c) Technische Universität Berlin, innoCampus <info@isis.tu-berlin.de>
@@ -13,7 +13,7 @@ import prettier from 'prettier'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-const bannerComment = `/**
+const bannerComment = `/*
  * This file is part of the QuestionPy SDK. (https://questionpy.org)
  * The QuestionPy SDK is free software released under terms of the MIT license. See LICENSE.md.
  * (c) Technische Universität Berlin, innoCampus <info@isis.tu-berlin.de>
