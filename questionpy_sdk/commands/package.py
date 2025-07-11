a037f1e8edd582c1d27156df8715daee9125c355
Maximilian Haye <m.haye@tu-berlin.de>
2025-07-11 16:55:08 +0200

    refactor: use a temp file in ZipBuildTarget
    We already used a temp file, but that was previously part of the CLI code.

--- questionpy_sdk/commands/package.py
+++ questionpy_sdk/commands/package.py
@@ -1,9 +1,6 @@
 #  This file is part of the QuestionPy SDK. (https://questionpy.org)
 #  The QuestionPy SDK is free software released under terms of the MIT license. See LICENSE.md.
 #  (c) Technische Universität Berlin, innoCampus <info@isis.tu-berlin.de>
-import shutil
-import tempfile
-from contextlib import suppress
 from pathlib import Path
 
 import click
@@ -45,14 +42,14 @@ def validate_out_path(context: click.Context, _parameter: click.Parameter, value
     is_flag=True,
     help="Don't copy package sources into the .qpy file.",
 )
-@click.option("--force", "-f", "force_overwrite", is_flag=True, help="Force overwriting of output file.")
+@click.option("--force", "-f", "allow_overwrite", is_flag=True, help="Force overwriting of output file.")
 @click.pass_context
 def package(
     ctx: click.Context,
     source: Path,
     out_path: Path | None,
     *,
-    force_overwrite: bool,
+    allow_overwrite: bool,
     development: bool,
     without_sources: bool,
 ) -> None:
@@ -65,7 +62,7 @@ def package(
     for param_name, param in (
         ("--out", out_path),
         ("--without-sources", without_sources),
-        ("--force", force_overwrite),
+        ("--force", allow_overwrite),
     ):
         if param and development:
             msg = f"The options {param_name} and --dev are mutually exclusive."
@@ -78,7 +75,7 @@ def package(
         if not out_path:
             out_path = Path(package_source.normalized_filename)
         create_qpy_package(
-            ctx, package_source, out_path, force_overwrite=force_overwrite, without_sources=without_sources
+            ctx, package_source, out_path, allow_overwrite=allow_overwrite, without_sources=without_sources
         )
 
 
@@ -93,36 +90,21 @@ def create_dist(package_source: PackageSource) -> None:
 
 
 def create_qpy_package(
-    ctx: click.Context, package_source: PackageSource, out_path: Path, *, force_overwrite: bool, without_sources: bool
+    ctx: click.Context, package_source: PackageSource, out_path: Path, *, allow_overwrite: bool, without_sources: bool
 ) -> None:
-    overwriting = False
-
-    if out_path.exists():
-        if force_overwrite or (not ctx.obj["no_interaction"] and confirm_overwrite(out_path)):
-            overwriting = True
+    if out_path.exists() and not allow_overwrite:
+        if not ctx.obj["no_interaction"]:
+            allow_overwrite = confirm_overwrite(out_path)
         else:
             msg = f"Output file '{out_path}' exists"
             raise click.ClickException(msg)
 
     try:
-        # Use temp file, otherwise we risk overwriting `out_path` in case of a build error.
-        temp_file = tempfile.NamedTemporaryFile(delete=False)  # noqa: SIM115
-        temp_file_path = Path(temp_file.name)
-
-        try:
-            build_qpy_package(package_source, ZipBuildTarget(temp_file_path), copy_sources=not without_sources)
-        except PackageBuildError as exc:
-            msg = f"Failed to build package: {exc}"
-            raise click.ClickException(msg) from exc
-        finally:
-            temp_file.close()
-
-        if overwriting:
-            Path(out_path).unlink()
-
-        shutil.move(temp_file_path, out_path)
-    finally:
-        with suppress(FileNotFoundError):
-            temp_file_path.unlink()
+        build_qpy_package(
+            package_source, ZipBuildTarget(out_path, allow_overwrite=allow_overwrite), copy_sources=not without_sources
+        )
+    except PackageBuildError as exc:
+        msg = f"Failed to build package: {exc}"
+        raise click.ClickException(msg) from exc
 
     click.echo(f"Successfully created '{out_path}'.")
--- tests/questionpy_sdk/webserver/test_webserver.py
+++ tests/questionpy_sdk/webserver/test_webserver.py
@@ -15,8 +15,7 @@ from questionpy import Attempt, NeedsManualScoringError, Package, Question, Ques
 from questionpy.form import FormModel
 from questionpy_common.api.qtype import QuestionTypeInterface
 from questionpy_common.constants import DIST_DIR
-
-from questionpy_sdk.package import build_qpy_package, DirBuildTarget
+from questionpy_sdk.package import DirBuildTarget, build_qpy_package
 from questionpy_sdk.package.source import PackageSource
 from questionpy_sdk.webserver.server import WebServer
 from questionpy_server.hash import calculate_hash
