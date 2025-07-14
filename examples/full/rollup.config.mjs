5f4d0ef66588909cbd20c05def155f5ca24f1ab3
Maximilian Haye <m.haye@tu-berlin.de>
2025-07-14 15:05:33 +0200

    build: write the example rollup.config.mjs in a way that supports non-qpy builds

--- examples/full/rollup.config.mjs
+++ examples/full/rollup.config.mjs
@@ -2,12 +2,17 @@ import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import sass from "rollup-plugin-sass";
 import { defineConfig } from "rollup";
+import path from "node:path"
+import process from "node:process"
+
+const jsOutputPath = process.env.QPY_DIST_JS ?? path.resolve(import.meta.dirname, "dist", "static", "js");
+const cssOutputPath = process.env.QPY_DIST_CSS ?? path.resolve(import.meta.dirname, "dist", "static", "css");
 
 export default defineConfig({
     input: "src/main.ts",
     output: {
-        file: `${process.env.QPY_DIST_JS}/main.js`,
+        file: `${jsOutputPath}/main.js`,
         format: "amd",
     },
-    plugins: [typescript(), sass({ output: `${process.env.QPY_DIST_CSS}/styles.css` }), terser()],
+    plugins: [typescript(), sass({ output: `${cssOutputPath}/styles.css` }), terser()],
 });
