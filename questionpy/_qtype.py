b57e975180fb2b696615a71a05cb2c76efb16fd7
Maximilian Haye <m.haye@tu-berlin.de>
2025-07-29 19:42:18 +0200

    refactor: use get_new_question_options_form in get_options_form
    In most cases, the form won't depend on the question state, so this makes it easier to customize it.

--- questionpy/_qtype.py
+++ questionpy/_qtype.py
@@ -145,7 +145,7 @@ class Question(ABC):
 
     def get_options_form(self) -> tuple[OptionsFormDefinition, dict[str, JsonValue]]:
         """Return the options form and field values for viewing or editing this question."""
-        return self.options_class.qpy_form, self.options.model_dump(mode="json")
+        return self.get_new_question_options_form(), self.options.model_dump(mode="json")
 
     def start_attempt(self, variant: int) -> AttemptStartedProtocol:
         attempt_state = self.attempt_class.make_attempt_state(self, variant)
